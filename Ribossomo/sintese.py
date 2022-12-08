9f1c088287db6d82fe7236a0d705c96c830fe49d
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-12-08 12:03:36 -0300

    sintese

---
+++ Ribossomo/sintese.py
@@ -0,0 +1,20 @@
+def readInput(nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+        fita = arquivo.read()
+    return fita
+
+def writeData(nome_arquivo, dados):
+    with open(nome_arquivo, 'w', encoding='utf-8') as arquivo:
+        arquivo.write(dados)
+
+def tratamentoEntrada(entrada):
+    entrada = entrada.strip().replace('\n', '')
+    return entrada
+
+def sintese():
+    dados = readInput('entrada.txt')
+    condicoes_parada = ['UAA', 'UAG', 'UGA']
+    ...
+
+
+sintese()
