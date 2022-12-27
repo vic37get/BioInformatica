e2323f2d17328396c1cecd00f53daaec2dbc89a0
Victor <victorribeiro172@gmail.com>
2022-12-27 17:32:33 -0300

    criação da matriz com gaps

--- SmithWaterman/smithwaterman.py
+++ SmithWaterman/smithwaterman.py
@@ -24,20 +24,42 @@ def trataInput(dados):
     }
     return info
 
-def smithWaterman(sequenciaUm, sequenciaDois, match, mismatch, gap):
+def montaMatriz(sequenciaUm, sequenciaDois):
     matriz = np.zeros((len(sequenciaUm) + 1, len(sequenciaDois) + 1))
-    for i in range(1, matriz.shape[0]):
+    gap = 0
+    for linha in range(1, matriz.shape[1]):
+        gap-=2
+        matriz[-1][linha] = gap
+    gap = 0
+    for coluna in range(matriz.shape[0]-2, -1, -1):
+        gap-=2
+        matriz[coluna][0] = gap
+    return matriz
+
+def smithWaterman(sequenciaUm, sequenciaDois, match, mismatch, gap):
+    sequenciaUm = sequenciaUm[::-1]
+    matriz = montaMatriz(sequenciaUm, sequenciaDois)
+
+    #Olhar como ele percorre
+    
+    '''for i in range(matriz.shape[0]-1, -1, -1):
         for j in range(1, matriz.shape[1]):
-            score_match, score_gap, score_gap_insert = float('-inf'), float('-inf'), float('-inf')
+            score_match, sequenciaUm_gap, sequenciaDois_gap = float('-inf'), float('-inf'), float('-inf')
+            print(sequenciaUm[i - 1], sequenciaDois[j - 1])
             if sequenciaUm[i - 1] == sequenciaDois[j - 1]:
                 score_match = matriz[i - 1][j - 1] + int(match)
+                print('ScoreMatch: ', score_match)
+                print(matriz)
             else:
-                score_gap = matriz[i - 1][j] + int(gap)
-                print('foi gap!')
-                score_gap_insert = matriz[i][j - 1] + int(gap)
-            print(sequenciaUm[i - 1], sequenciaDois[j - 1])
-            matriz[i, j] = max(score_match, score_gap, score_gap_insert)
-            print(matriz)
+                print('SequenciaUm_gap: ', matriz[i - 1][j])
+                sequenciaUm_gap = matriz[i - 1][j] + int(gap)
+                print('SequenciaDois_gap: ', matriz[i][j - 1])
+                sequenciaDois_gap = matriz[i][j - 1] + int(gap)
+                score_mismatch = matriz[i - 1][j - 1] + int(mismatch)
+                print(matriz)
+            matriz[i, j] = max(score_match, sequenciaUm_gap, sequenciaDois_gap, score_mismatch)
+            print('Matriz alterada:')
+            print(matriz)'''
 
 
 info = abreInput('input.txt')
